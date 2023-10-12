d0975d763a7170974e3cbca0615e151211fb72f6
Niels van Brenk <niels.van.brenk@harvest.nl>
2023-10-12 14:03:19 +0200

    theme change

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,9 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// Gebruik `workbox.routing.registerRoute()`...
-
+// Gebruik `workbox.routing.registerRoute()`..
+navigator.serviceWorker.register('/service-worker.js', {
+    scope: '/'
+}).then(function (registration) {
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+})
